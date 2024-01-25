ecad65096f404f23486dae83c08db73ff9977c9b
Amanda Chaves <101990484+keysgc@users.noreply.github.com>
2024-01-24 22:10:57 -0300

    revisao

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 /**Encontrar a seção com a classe '.card-pokemon' */
 
-const secaoPokemon = document.querySelector('.card-pokemon')
-console.log(secaoPokemon
+const secaoPokemon = document.querySelector('.secao-pokemon')
+console.log(secaoPokemon) 
 
 function criarCard(){
 
 /** // Criar a div pai 'card-info' */
-   
+
+
 	let cardPokemon = document.createElement('div') 
-	cardPokemon.classList.add('card-info')
+	cardPokemon.classList.add('grass-card')
+	cardPokemon.classList.add('pokecard') 
+
+
+/** // Criar a div pai 'card-info' */
+   
+	let cardInfo = document.createElement('div') 
+	cardInfo.classList.add('card-info')
     
 /** Criar a div 'pokename'**/
 	
@@ -31,16 +39,21 @@ function criarCard(){
 	nomePokemon.appendChild(codigoId)
 	nomePokemon.appendChild(textName)
     
+
 /** Adicionar a div 'pokename' ao div pai 'card-info' */
   
-	cardPokemon.appendChild(nomePokemon)
+	cardInfo.appendChild(nomePokemon)
 
-/** Adicionar a div pai 'card-info' à seção '.card-pokemon' */
+/** Adicionar a div 'cardInfo' ao div pai 'cardPokemon' */
+
+	cardPokemon.appendChild(cardInfo)
+
+/** Adicionar a div pai 'card-info' à seção '.secao-pokemon' */
     
 	secaoPokemon.appendChild(cardPokemon);
 
 
-  
+
 					/* Adicionar imagem:
 					 
 					 let imagem = document.createElement('img')
@@ -75,16 +88,18 @@ function criarCard(){
 	poketype.classList.add('poketype')
 
 
+
+
 /** Criar a div do tipo 'grass' */
 
-	let grassType = document.createElement('div')
+	grassType = document.createElement('div')
 	grassType.id = 'grass'
 	grassType.classList.add('type')
 
 /** Adicionar a imagem do tipo 'grass' */
 
 	let leafImage = document.createElement('img')
-	leafImage.setAttribute('scr', './img/foia.png')
+	leafImage.setAttribute('src', './img/foia.png')
 	console.log(leafImage)
 
 /** Criar o p 'Grass' para o nome do tipo do pokemon */
@@ -93,41 +108,45 @@ function criarCard(){
 	typeName.textContent = 'Grass'
 
 
+	grassType.appendChild(leafImage)
+	grassType.appendChild(typeName)
 
 
 /** Criar a div do tipo 'poison' */
 
-	let poisonType = document.createElement('div')
+	poisonType = document.createElement('div')
 	poisonType.id = 'poison'
 	poisonType.classList.add('type')
 
 /** Adicionar a imagem do tipo 'poison' */
 
 	let poisonImage = document.createElement('img')
-	poisonImage.setAttribute('scr', './img/Poison.png')
+	poisonImage.setAttribute('src', './img/Poison.png')
 	console.log(poisonImage)
 
 /** Criar o p 'Poison' para o nome do tipo do pokemon */
 
-	let typeName = document.createElement('p')
+	typeName = document.createElement('p')
 	typeName.textContent = 'Poison'
 
 
 /** Adicionar os tipos à div 'poketype' */
- 
+	poisonType.appendChild(poisonImage)
+	poisonType.appendChild(typeName)
 	poketype.appendChild(grassType)
 	poketype.appendChild(poisonType)
 
+	cardInfo.appendChild(poketype)
 
 /** Criar a div 'pokeball-img' */
 
 	let pokeballImage = document.createElement('div')
-	pokeballImagem.classList.add('pokeball-img')
+	pokeballImage.classList.add('pokeball-img')
 
 /** Adicionar a imagem de pokebola ao card pokemon */
 
 	let pokeballImg = document.createElement('img')
-	pokeballImg.setAattribute('src', './img/Pokeball.png')
+	pokeballImg.setAttribute('src', './img/Pokeball.png')
 	console.log(pokeballImg) 
 
 
@@ -142,335 +161,65 @@ function criarCard(){
 	bulbaImage.setAttribute('src', './img/01.png')
 	console.log(bulbaImage)
 
+	pokeballImage.appendChild(pokeballImg)
+	cardPokemon.appendChild(pokeballImage)
 
+	pokemonImage.appendChild(bulbaImage)
+	cardPokemon.appendChild(pokemonImage)
 
-
-
-/**     <!-- Ivysaur -->    */ 
-
-
-
-/** // Criar a div pai 'card-info' */
-   
-	let cardPokemon = document.createElement('div') 
-	cardPokemon.classList.add('card-info')
-    
-/** Criar a div 'pokename'**/
-	
-	let nomePokemon = document.createElement('div')
-	nomePokemon.classList.add('pokename')
-    
-/** Criar o span 'id' para o código do Pokemon **/
-
-	let codigoId = document.createElement('span')
-	codigoId.classList.add('id')
-	codigoId.textContent = '#002'
-
-/** Criar o h1 para o nome do Pokemon */
-
-	let textName = document.createElement('h1')
-	textName.textContent = 'Ivysaur'
-
-/** Adicionar o span 'id' e o h1 ao div 'pokename' */
-
-	nomePokemon.appendChild(codigoId)
-	nomePokemon.appendChild(textName)
-    
-/** Adicionar a div 'pokename' ao div pai 'card-info' */
-  
-	cardPokemon.appendChild(nomePokemon)
-
-/** Adicionar a div pai 'card-info' à seção '.card-pokemon' */
-    
-	secaoPokemon.appendChild(cardPokemon);
-
-
-/** Criar a div 'poketype' */
-    
-	let poketype = document.createElement('div')
-	poketype.classList.add('poketype')
-
-
-/** Criar a div do tipo 'grass' */
-
-	let grassType = document.createElement('div')
-	grassType.id = 'grass'
-	grassType.classList.add('type')
-
-/** Adicionar a imagem do tipo 'grass' */
-
-	let leafImage = document.createElement('img')
-	leafImage.setAttribute('scr', './img/foia.png')
-	console.log(leafImage)
-
-/** Criar o p 'Grass' para o nome do tipo do pokemon */
-
-	let typeName = document.createElement('p')
-	typeName.textContent = 'Grass'
-
-
-
-/** Criar a div do tipo 'poison' */
-
-	let poisonType = document.createElement('div')
-	poisonType.id = 'poison'
-	poisonType.classList.add('type')
-
-/** Adicionar a imagem do tipo 'poison' */
-
-	let poisonImage = document.createElement('img')
-	poisonImage.setAttribute('scr', './img/Poison.png')
-	console.log(poisonImage)
-
-/** Criar o p 'Poison' para o nome do tipo do pokemon */
-
-	let typeName = document.createElement('p')
-	typeName.textContent = 'Poison'
-
-
-/** Adicionar os tipos à div 'poketype' */
- 
-	poketype.appendChild(grassType)
-	poketype.appendChild(poisonType)
-
-
-/** Criar a div 'pokeball-img' */
-
-	let pokeballImage = document.createElement('div')
-	pokeballImagem.classList.add('pokeball-img')
-
-/** Adicionar a imagem de pokebola ao card pokemon */
-
-	let pokeballImg = document.createElement('img')
-	pokeballImg.setAattribute('src', './img/Pokeball.png')
-	console.log(pokeballImg) 
-
-
-/** Adicionar a div da imagem do pokemon Ivysaur ao card */
-
-	let pokemonImage = document.createElement('div')
-	pokemonImage.classList.add('poke-img')
-
-/** Adicionar a imagem do pokemon Ivysaur ao card */
-
-	let ivysaurImage = document.createElement('img')
-	ivysaurImage.setAttribute('src', './img/01.png')
-	console.log(ivysaurImage)
-
-
-
-
-/**     <!-- Venusaur -->    */ 
-
-
-
-/** // Criar a div pai 'card-info' */
-   
-	let cardPokemon = document.createElement('div') 
-	cardPokemon.classList.add('card-info')
-    
-/** Criar a div 'pokename'**/
-	
-	let nomePokemon = document.createElement('div')
-	nomePokemon.classList.add('pokename')
-    
-/** Criar o span 'id' para o código do Pokemon **/
-
-	let codigoId = document.createElement('span')
-	codigoId.classList.add('id')
-	codigoId.textContent = '#003'
-
-/** Criar o h1 para o nome do Pokemon */
-
-	let textName = document.createElement('h1')
-	textName.textContent = 'Venusaur'
-
-/** Adicionar o span 'id' e o h1 ao div 'pokename' */
-
-	nomePokemon.appendChild(codigoId)
-	nomePokemon.appendChild(textName)
-    
-/** Adicionar a div 'pokename' ao div pai 'card-info' */
-  
-	cardPokemon.appendChild(nomePokemon)
-
-/** Adicionar a div pai 'card-info' à seção '.card-pokemon' */
-    
-	secaoPokemon.appendChild(cardPokemon);
-
-
-/** Criar a div 'poketype' */
-    
-	let poketype = document.createElement('div')
-	poketype.classList.add('poketype')
-
-
-/** Criar a div do tipo 'grass' */
-
-	let grassType = document.createElement('div')
-	grassType.id = 'grass'
-	grassType.classList.add('type')
-
-/** Adicionar a imagem do tipo 'grass' */
-
-	let leafImage = document.createElement('img')
-	leafImage.setAttribute('scr', './img/foia.png')
-	console.log(leafImage)
-
-/** Criar o p 'Grass' para o nome do tipo do pokemon */
-
-	let typeName = document.createElement('p')
-	typeName.textContent = 'Grass'
-
-
-
-/** Criar a div do tipo 'poison' */
-
-	let poisonType = document.createElement('div')
-	poisonType.id = 'poison'
-	poisonType.classList.add('type')
-
-/** Adicionar a imagem do tipo 'poison' */
-
-	let poisonImage = document.createElement('img')
-	poisonImage.setAttribute('scr', './img/Poison.png')
-	console.log(poisonImage)
-
-/** Criar o p 'Poison' para o nome do tipo do pokemon */
-
-	let typeName = document.createElement('p')
-	typeName.textContent = 'Poison'
-
-
-/** Adicionar os tipos à div 'poketype' */
- 
-	poketype.appendChild(grassType)
-	poketype.appendChild(poisonType)
-
-
-/** Criar a div 'pokeball-img' */
-
-	let pokeballImage = document.createElement('div')
-	pokeballImagem.classList.add('pokeball-img')
-
-/** Adicionar a imagem de pokebola ao card pokemon */
-
-	let pokeballImg = document.createElement('img')
-	pokeballImg.setAattribute('src', './img/Pokeball.png')
-	console.log(pokeballImg) 
-
-
-/** Adicionar a div da imagem do pokemon Venusaur ao card */
-
-	let pokemonImage = document.createElement('div')
-	pokemonImage.classList.add('poke-img')
-
-/** Adicionar a imagem do pokemon Venusaur ao card */
-
-	let venusaurImage = document.createElement('img')
-	venusaurImage.setAttribute('src', './img/venusaur.png')
-	console.log(venusaurImage)
-
-
-
-
-
-
-/**     <!-- Charmander -->    */ 
-
-
-
-/** // Criar a div pai 'card-info' */
-   
-	let cardPokemon = document.createElement('div') 
-	cardPokemon.classList.add('card-info')
-    
-/** Criar a div 'pokename'**/
-	
-	let nomePokemon = document.createElement('div')
-	nomePokemon.classList.add('pokename')
-    
-/** Criar o span 'id' para o código do Pokemon **/
-
-	let codigoId = document.createElement('span')
-	codigoId.classList.add('id')
-	codigoId.textContent = '#004'
-
-/** Criar o h1 para o nome do Pokemon */
-
-	let textName = document.createElement('h1')
-	textName.textContent = 'Charmander'
-
-/** Adicionar o span 'id' e o h1 ao div 'pokename' */
-
-	nomePokemon.appendChild(codigoId)
-	nomePokemon.appendChild(textName)
-    
-/** Adicionar a div 'pokename' ao div pai 'card-info' */
-  
-	cardPokemon.appendChild(nomePokemon)
-
-/** Adicionar a div pai 'card-info' à seção '.card-pokemon' */
-    
-	secaoPokemon.appendChild(cardPokemon);
-
-
-/** Criar a div 'poketype' */
-    
-	let poketype = document.createElement('div')
-	poketype.classList.add('poketype')
-
-
-/** Criar a div do tipo 'fire' */
-
-	let fireType = document.createElement('div')
-	fireType.id = 'fire'
-	fireType.classList.add('type')
-
-/** Adicionar a imagem do tipo 'fire' */
-
-	let fireImage = document.createElement('img')
-	fireImage.setAttribute('scr', './img/Fire.png')
-	console.log(firefImage)
-
-/** Criar o p 'Fire' para o nome do tipo do pokemon */
-
-	let typeName = document.createElement('p')
-	typeName.textContent = 'Fire'
-
-
-
-/** Adicionar os tipos à div 'poketype' */
- 
-	poketype.appendChild(fireType)
-
-
-
-/** Criar a div 'pokeball-img' */
-
-	let pokeballImage = document.createElement('div')
-	pokeballImagem.classList.add('pokeball-img')
-
-/** Adicionar a imagem de pokebola ao card pokemon */
-
-	let pokeballImg = document.createElement('img')
-	pokeballImg.setAattribute('src', './img/Pokeball.png')
-	console.log(pokeballImg) 
-
-
-/** Adicionar a div da imagem do pokemon Charmander ao card */
-
-	let pokemonImage = document.createElement('div')
-	pokemonImage.classList.add('poke-img')
-
-/** Adicionar a imagem do pokemon Charmander ao card */
-
-	let charmanderImage = document.createElement('img')
-	charmanderImage.setAttribute('src', './img/charmander.png')
-	console.log(charmanderImage)
 
 }
+
+
 
 /** Chamar a função para criar o card */
     
 criarCard()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
